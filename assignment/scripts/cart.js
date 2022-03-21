2017e9ec32e02b25dda679b5a19dc4c4ee18d13f
Chad Rhiger <csrhig@gmail.com>
2022-03-21 12:20:59 -0500

    created empty basket, pushed garlic into it. current addItem function only verifies that an item has been added. will continue to tweak

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,27 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket =[];
+basket.push('galric');
+
+function addItem(){
+    basket.push()
+    for(i of basket){
+        return true;
+    } 
+    return
+}
+
+function listItems(){
+
+
+}
+
+function empty(){
+
+}
+
+console.log(`Basket is ${basket}`);
+console.log('Adding garlic (expect true)', addItem());
+console.log(`Basket is now ${basket}`);
