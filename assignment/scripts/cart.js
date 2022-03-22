16d332a6db1ceb2f8ec3b1239345e2cc0d0bddfb
Chad Rhiger <csrhig@gmail.com>
2022-03-22 10:50:02 -0500

    toyed with stretch goals, commented them out.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,16 +3,17 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 let basket =[];
+// const maxItems = 5;
 
 function addItem(itemName) {
     basket.push('onions');
-        if (itemName === 'onions'){
+        if (itemName = 'onions'){
             return true;
         }
 }
 
 console.log(`Basket is ${basket}`); 
-console.log('Adding onions (expect true)', addItem('onions'));
+console.log('Adding onions (expect true)', addItem());
 console.log(`Basket is now ${basket}`);
 
 basket.push('celery');
@@ -21,14 +22,29 @@ basket.push('green peppers');
 
 function listItems(){
     for(let item of basket) {
-        console.log(item);
+        console.log('Thru unholy computer sorcery, the basket now contains:', item);
     }
 }
-listItems()
+listItems();
 
 function empty(){
     let basket = [];
-    console.log('Basket is empty');
-    
+    console.log('Basket has been emptied.');   
 }    
-empty()
+empty();
+
+
+
+// function isFull(){
+//     for (items of basket){
+//         if (items < maxItems){
+//             return false;
+//         }
+//         else if (items >= maxItems){
+//             return true;
+//         }
+//     }    
+// }
+// console.log('Basket is full:', isFull());
+
+
